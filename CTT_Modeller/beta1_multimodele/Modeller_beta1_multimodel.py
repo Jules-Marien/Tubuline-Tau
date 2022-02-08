77e013b3d7dd37cb5fd86cbd0327c806241f8668
Jules-Marien <98336603+Jules-Marien@users.noreply.github.com>
2022-02-08 16:39:34 +0100

    Add files via upload

---
+++ CTT_Modeller/beta1_multimodele/Modeller_beta1_multimodel.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Feb  1 17:18:27 2022
+
+@author: marien
+"""
+
+
+# Comparative modeling by the AutoModel class
+from modeller import *              # Load standard Modeller classes
+from modeller.automodel import *    # Load the AutoModel class
+
+log.verbose()    # request verbose output
+env = Environ()  # create a new MODELLER environment to build this model in
+
+# directories for input atom files
+env.io.atom_files_directory = ['.']
+
+
+a = AutoModel(env,
+              alnfile  = 'alignement_beta1_1SA0.ali',     # alignment filename
+              knowns   = '1SA0',              # codes of the templates
+              sequence = 'beta1',             # code of the target
+              assess_methods=(assess.DOPE,
+                              #soap_protein_od.Scorer(),
+                              assess.GA341))             #Permettent d'obtenir une évaluation des modèles générés
+
+      
+
+a.starting_model= 1                 # index of the first model
+a.ending_model  = 5                 # index of the last model
+                                    # (determines how many models to calculate)
+a.make()                            # do the actual comparative modeling
